e79928ee4dabaa9b97e81c9c6978704fc72428a2
Thomas Plans <Thomas Plans>
2016-06-18 14:26:47 +0100

    V 0.6 - Fixed Bot

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='slackbottools',
-      version='0.5',
+      version='0.6',
       description='A python package built for writing SlackBots',
       url='https://github.com/coderplans/SlackBot',
       author='coderplans',
