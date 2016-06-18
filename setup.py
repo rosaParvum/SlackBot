a62fed3391b827950b57d6244d7807ebd61dc5b5
Thomas Plans <Thomas Plans>
2016-06-18 10:54:02 +0100

    V 0.3

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='slackbot-tools',
-      version='0.1',
+setup(name='slackbottools',
+      version='0.3',
       description='A python package built for writing SlackBots',
       url='https://github.com/coderplans/SlackBot',
       author='coderplans',
       author_email='thomas@khandaan.org',
       license='Apache 2.0',
-      packages=['slackbot-tools'],
+      packages=['slackbottools'],
       zip_safe=False)
