018a16d6ed552ab5ced15f68cceefd8e42e792ba
Thomas Plans <Thomas Plans>
2016-06-18 09:50:20 +0100

    PyPy 1

---
+++ SlackBot/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='slackbot',
+      version='0.1',
+      description='A python package built for writing SlackBots',
+      url='https://github.com/coderplans/SlackBot',
+      author='coderplans',
+      author_email='thomas@khandaan.org',
+      license='Apache 2.0',
+      packages=['slackbot'],
+      zip_safe=False)
