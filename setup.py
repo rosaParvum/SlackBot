b5a4d7ab5cee6c26966140cfeb1f3119dbdd60a5
Thomas Plans <Thomas Plans>
2016-06-18 10:31:37 +0100

    PyPi 2

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='slackbot',
+setup(name='slackbot-tools',
       version='0.1',
       description='A python package built for writing SlackBots',
       url='https://github.com/coderplans/SlackBot',
       author='coderplans',
       author_email='thomas@khandaan.org',
       license='Apache 2.0',
-      packages=['slackbot'],
+      packages=['slackbot-tools'],
       zip_safe=False)
